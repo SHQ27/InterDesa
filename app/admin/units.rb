e777f1937aab93c8a86137c336dcf5a5e933d557
SHQ <sebastian@idiotlab.io>
2021-09-15 02:35:17 -0300

    Unit no se puede editar si ya tiene un PaymentPlan asociado

--- app/admin/units.rb
+++ app/admin/units.rb
@@ -3,13 +3,14 @@ ActiveAdmin.register Unit do
   menu priority: 2, parent: "Buildings" 
 
   form do |f|
+    disabled = f.object && f.object.payment_plan ? true : false
+    selectedType = f.object ? f.object.type_id : 'apartment'
      f.inputs 'Information' do
-       f.input :name
-       f.input :building_id, as: :select, collection: Building.all, label: :name
-       f.input :type_id, as: :select, collection: Unit.getTypes, label: :name, selected: 'apartment'
-       f.input :price
-       f.input :details, label: 'Details'
-
+       f.input :name, :input_html => { :disabled => disabled }
+       f.input :building_id, as: :select, collection: Building.all, label: :name, :input_html => { :disabled => disabled }
+       f.input :type_id, as: :select, collection: Unit.getTypes, label: :name, selected: selectedType, :input_html => { :disabled => disabled }
+       f.input :price, :input_html => { :disabled => disabled }
+       f.input :details, label: 'Details', :input_html => { :disabled => disabled }
      end
      f.actions
    end
